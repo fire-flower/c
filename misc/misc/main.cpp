1e330f4fe639dcdfd816e7ba6a3de1bc2068e1a8
fireflower <choicefun72@gmail.com>
2019-12-15 16:50:34 +0900

    출력 추가 :  종료 문구
    종료 문구 출력 추가

--- misc/misc/main.cpp
+++ misc/misc/main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main(int argc, char* args[])
 {
 	cout << "This project is to verify c/c++ test." << endl;
+	cou << "End  program : Normal terminated! Good for you!" << endl;
 	return 0;
 }
